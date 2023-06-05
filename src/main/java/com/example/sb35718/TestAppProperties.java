73c2a97276c0e457d7ca0a50067fb43a632e5dab
Moritz Halbritter <mkammerer@vmware.com>
2023-06-05 10:34:40 +0200

    Change string to path

--- src/main/java/com/example/sb35718/TestAppProperties.java
+++ src/main/java/com/example/sb35718/TestAppProperties.java
@@ -2,18 +2,20 @@ package com.example.sb35718;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.nio.file.Path;
+
 /**
  * @author Moritz Halbritter
  */
 @ConfigurationProperties(prefix = "mh")
 public class TestAppProperties {
-    private String home;
+    private Path home;
 
-    public String getHome() {
+    public Path getHome() {
         return this.home;
     }
 
-    public void setHome(String home) {
+    public void setHome(Path home) {
         this.home = home;
     }
 }
