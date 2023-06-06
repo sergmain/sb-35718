98d460ac2a987c097da22f6fbe94b674224caa4b
sergmain <serg_main@yahoo.com>
2023-06-05 19:29:05 -0700

    .

--- src/main/java/com/example/sb35718/CLR.java
+++ src/main/java/com/example/sb35718/CLR.java
@@ -1,7 +1,9 @@
 package com.example.sb35718;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,14 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableConfigurationProperties(TestAppProperties.class)
 class CLR implements CommandLineRunner {
+    private final ApplicationContext context;
     private final TestAppProperties properties;
 
-    CLR(TestAppProperties properties) {
+    CLR(ApplicationContext context, TestAppProperties properties) {
+        this.context = context;
         this.properties = properties;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         System.out.println(this.properties.getHome());
+        System.exit(SpringApplication.exit(context, ()->0));
     }
 }
--- src/main/java/com/example/sb35718/TestAppProperties.java
+++ src/main/java/com/example/sb35718/TestAppProperties.java
@@ -1,6 +1,7 @@
 package com.example.sb35718;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
 
 import java.nio.file.Path;
 
@@ -10,6 +11,12 @@ import java.nio.file.Path;
 @ConfigurationProperties(prefix = "mh")
 public class TestAppProperties {
     private Path home;
+
+    private final Environment env;
+
+    public TestAppProperties(Environment env) {
+        this.env = env;
+    }
 
     public Path getHome() {
         return this.home;
